2b70fccb1e4f52495d2113ca6276f69421b92675
Vinicius Rocha <vnc.chelsea@gmail.com>
2018-04-18 00:33:04 -0300

    Primeiro teste de rota

--- src/api/routers/test-router.js
+++ src/api/routers/test-router.js
@@ -3,11 +3,12 @@ const express = require('express')
 module.exports = function(server){
 
     // Definir URL base para todas as rotas
-    const router = express.Router()
-    
+    const router = express.Router()   
     server.use('/api', router)
+    
+    const test_service = require('../service/test-service')
 
+    
     // Rotas
-    const test = require('../service/test-service')
-    test.register(router, '/test')
+    test_service.register(router, '/test')
 }
