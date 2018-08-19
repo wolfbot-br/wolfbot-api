2777a1584b452a35f90d8de5f917f752bff7917c
Celso Fernandes <celsojfernandes96@gmail.com>
2018-08-19 16:03:46 -0300

    fix: corrigindo nome da collection no banco de dados

--- src/infraestrutura/mongo/models/configuracao.model.js
+++ src/infraestrutura/mongo/models/configuracao.model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 
-const eschangeTokensSchema = new mongoose.Schema({
+const configuracao = new mongoose.Schema({
   api_key: { type: String, require: true },
   secret: { type: String, require: true },
   usuario: {},
   exchange: {},
-  status: { type: String, require: true }
+  status: { type: String, require: true },
+  chave: { type: String, require: true }
 })
 
-module.exports = mongoose.model('exchanges_tokens', eschangeTokensSchema)
+/* o nome da colection está configuracoe, porque quando cria a collection no banco ele coloca um s, deixando no plural*/
+module.exports = mongoose.model('configuracoe', configuracao)
