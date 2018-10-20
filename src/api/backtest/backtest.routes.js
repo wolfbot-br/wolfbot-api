09f81f94b2a9b8a6349e6ffd995b5aa4cd93ad75
allyson316 <allyson316@gmail.com>
2018-10-19 22:23:01 -0300

    ajustes backtest

--- src/api/backtest/backtest.routes.js
+++ src/api/backtest/backtest.routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
+const auth = require('../../config/auth')
 
 module.exports = function (server) {
   const backtestController = require('../backtest/backtest.controller')
 
   const protectedRoutes = express.Router()
-  const openRoutes = express.Router()
 
-  // protectedRoutes.use(auth);
+  protectedRoutes.use(auth);
 
-  server.use('/backtest', openRoutes)
-  openRoutes.post('/testarConfiguracao', backtestController.testarConfiguracao)
+  server.use('/api', protectedRoutes)
+  protectedRoutes.post('/backtest/testarConfiguracao', backtestController.testarConfiguracao)
 }
