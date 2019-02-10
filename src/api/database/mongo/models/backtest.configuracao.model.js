a5b3519a94f0eacb90d86bc89227d9daa0406d9e
Vinícius Rocha <vnc.chelsea@gmail.com>
2019-02-10 21:17:56 -0200

    fix: resolvendo conflitos

--- src/api/database/mongo/models/backtest.configuracao.model.js
+++ src/api/database/mongo/models/backtest.configuracao.model.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const backtestConfiguracao = new mongoose.Schema({
-<<<<<<< HEAD:src/api/database/mongo/models/backtest.configuracao.model.js
     name: { type: String },
     exchange: { type: String },
     api_key: { type: String },
@@ -35,41 +34,6 @@ const backtestConfiguracao = new mongoose.Schema({
         },
     },
 });
-=======
-  name: { type: String },
-  exchange: { type: String },
-  api_key: { type: String },
-  secret: { type: String },
-  user: {
-    user_name: { type: String, require: true },
-    user_id: { type: String, require: true }
-  },
-  base_currency: { type: String },
-  target_currency: { type: String },
-  candle_size: { type: String },
-  sellForIndicator: { type: Boolean },
-  profit: { type: Number },
-  stop: { type: Number },
-  strategy: {
-    indicators: {
-      ema: {
-        status: { type: Boolean },
-        period: { type: Number }
-      },
-      macd: {
-        status: { type: Boolean },
-        shortPeriod: { type: Number },
-        longPeriod: { type: Number },
-        signalPeriod: { type: Number }
-      },
-      rsi: {
-        status: { type: Boolean },
-        period: { type: Number }
-      }
-    }
-  }
-})
->>>>>>> master:src/infraestrutura/mongo/models/backtest.configuracao.model.js
 
 export default mongoose.model(
     'backtest-configuracao',
